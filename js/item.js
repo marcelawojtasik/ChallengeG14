94474db99f0a0d5d943fa8a024f2df626e51da90
Marcela Wojtasik <marcelawojtasik@gmail.com>
2023-12-04 18:01:46 -0300

    Mission 3 completado, aunque faltan vistas admin

---
+++ js/item.js
@@ -0,0 +1,15 @@
+const agregar= document.querySelector('#agregar');
+const quitar= document.querySelector('#quitar');
+const quantity = document.querySelector('#quantity');
+
+agregar.addEventListener('click', () => quantity.value= Number(quantity.value)+1);
+
+quitar.addEventListener('click', () => {
+    if (quantity.value>1) {
+        quantity.value= Number(quantity.value)-1
+    } else {
+        quantity.value= 0
+    }
+});
+    
+/*Ver si la logica del contador no arruina algo*/
