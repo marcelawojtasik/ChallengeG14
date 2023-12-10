9851747932a0ee0db863751e0fd66ee0fcd5bd61
Marcela Wojtasik <marcelawojtasik@gmail.com>
2023-12-10 19:38:01 -0300

    Arreglo aspectos vistas

--- src/controllers/shop.controller.js
+++ src/controllers/shop.controller.js
@@ -2,8 +2,109 @@ const path = require('path');
 
 module.exports = {
     shop: (req,res) => {
+
+        const data = [
+            {
+            product_id: 1,
+            licence_name: "Pokemon",
+            category_name: "Figuras coleccionables",
+            product_name: "Pidgeotto",
+            product_description: "Figura coleccionable Pokemon",
+            product_price: 1799.99,
+            dues: 3,
+            product_sku: "PKM001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            {
+            product_id: 2,
+            licence_name: "Harry Potter",
+            category_name: "Figuras coleccionables",
+            product_name: "Hermione",
+            product_description: "Figura coleccionable Harry Potter",
+            product_price: 5799.99,
+            dues: 6,
+            product_sku: "HP0001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            {
+            product_id: 3,
+            licence_name: "Harry Potter",
+            category_name: "Figuras coleccionables",
+            product_name: "Luna Lovegood",
+            product_description: "Figura coleccionable Harry Potter",
+            product_price: 3799.99,
+            dues: 6,
+            product_sku: "HP0001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            {
+            product_id: 4,
+            licence_name: "Harry Potter",
+            category_name: "Figuras coleccionables",
+            product_name: "Harry Potter",
+            product_description: "Figura coleccionable Harry Potter",
+            product_price: 3799.99,
+            dues: 6,
+            product_sku: "HP0001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            {
+            product_id: 5,
+            licence_name: "Harry Potter",
+            category_name: "Figuras coleccionables",
+            product_name: "Luna Lovegood",
+            product_description: "Figura coleccionable Harry Potter",
+            product_price: 3799.99,
+            dues: 6,
+            product_sku: "HP0001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            {
+            product_id: 6,
+            licence_name: "Pokemon",
+            category_name: "Figuras coleccionables",
+            product_name: "Pidgeotto",
+            product_description: "Figura coleccionable Pokemon",
+            product_price: 1799.99,
+            dues: 3,
+            product_sku: "PKM001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            {
+            product_id: 7,
+            licence_name: "Harry Potter",
+            category_name: "Figuras coleccionables",
+            product_name: "Hermione",
+            product_description: "Figura coleccionable Harry Potter",
+            product_price: 5799.99,
+            dues: 6,
+            product_sku: "HP0001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            {
+            product_id: 8,
+            licence_name: "Harry Potter",
+            category_name: "Figuras coleccionables",
+            product_name: "Luna Lovegood",
+            product_description: "Figura coleccionable Harry Potter",
+            product_price: 3799.99,
+            dues: 6,
+            product_sku: "HP0001001",
+            img_front: "/img/pokemon/pidgeotto-1.webp",
+            img_back: "/img/pokemon/pidgeotto-box.webp"
+            },
+            ]
+
         res.render(path.resolve(__dirname, '../views/shop/shop.ejs'), {
-            title: "Shop | Funkoshop"
+            title: "Shop | Funkoshop", 
+            data
         });
     },
     cart: (req,res) => {
