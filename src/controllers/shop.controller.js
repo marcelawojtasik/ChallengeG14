d4c2e81c9f5ab1bae9af120d8b73776cded50c1b
Marcela Wojtasik <marcelawojtasik@gmail.com>
2023-12-13 21:43:46 -0300

    Marcela wojtasik (#11)
    * Destructuring en routes completo
    
    * Arreglo aspectos vistas
    
    * Mission 6 - Completo acceso a BD desde item, shop y edit(admin)
    
    * Vista edit dinamica en algunos campos, falta ajustar form
    
    * Acomode botones edit y create
    
    * Cart semi mejorado

--- src/controllers/shop.controller.js
+++ src/controllers/shop.controller.js
@@ -113,7 +113,6 @@ module.exports = {
             title: "Carrito de compras | Funkoshop"
         });
     },
-
     addToCart: (req,res) => res.send('Esta es la ruta para agregar un item al carrito - desde el Controller'),
     item: async (req,res) => {
         const itemId = req.params.id;
