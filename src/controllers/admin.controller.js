beb507cb91f37f270693bee29897a95a8e24fa15
Marcela Wojtasik <marcelawojtasik@gmail.com>
2023-12-16 19:10:23 -0300

    Marcela wojtasik (#12)
    * Destructuring en routes completo
    
    * Arreglo aspectos vistas
    
    * Mission 6 - Completo acceso a BD desde item, shop y edit(admin)
    
    * Vista edit dinamica en algunos campos, falta ajustar form
    
    * Acomode botones edit y create
    
    * Cart semi mejorado
    
    * Multer funcionando, probada la subidade rchivos
    
    * Multer y agregar items a BBDD funcionando

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const {getAll, getOne} = require('../models/product.model');
+const {getAll, getOne, doCreate} = require('../models/product.model');
 
 module.exports = {
     admin: async(req,res) => {
@@ -13,10 +13,33 @@ module.exports = {
     create: (req,res) => res.render(path.resolve(__dirname, '../views/admin/create.ejs'), {
         title: "Crear Item | Funkoshop"
     }),
-    doCreate: (req,res) => res.send('Esta es la ruta para Agregar un nuevo item'), /*asociada a boton agregar producto. Recibe datos de form en req*/
+
+    doCreate: async(req,res) => {        
+        console.log(req.body);
+        console.log(req.files);
+        
+        const product_schema = {
+            product_name: req.body.productName,
+            product_description: req.body.productDesc,
+            price: req.body.price,
+            stock: req.body.stock,
+            discount: req.body.discount,
+            sku: req.body.sku,
+            dues: req.body.dues,
+            image_front: req.files[0].originalname,
+            image_back: req.files[1].originalname,
+            category_id: req.body.category,
+            licence_id: req.body.licence
+        }
+        const result= await doCreate([Object.values(product_schema)]);
+        console.log("array: " [Object.values(product_schema)]);
+        console.log(result);    
+        res.redirect('/admin');    
+    },
+    /*res.send('Esta es la ruta para Agregar un nuevo item'), asociada a boton agregar producto. Recibe datos de form en req*/
     edit: async(req,res) => {
         const {id} = req.params;
-        const [product] = await getOne(id);
+        const [product] = await getOne({product_id : id}); /*Paso el objeto con el param q quiero q tome */
 
         res.render(path.resolve(__dirname, '../views/admin/edit.ejs'), {
         title: "Editar Item | Funkoshop",
